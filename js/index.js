6e349121fd22f3d316795b46e084e2369c61f647
Santiago Buenahora <santib97@gmail.com>
2017-02-03 17:38:00 -0500

    floor + working view controls

--- js/index.js
+++ js/index.js
@@ -43,7 +43,6 @@ if ( havePointerLock ) {
   element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock
     || element.webkitRequestPointerLock;
   element.requestPointerLock();
-  console.log("reaches");
   instructions.addEventListener( 'click', function ( event ) {
     instructions.style.display = 'none';
     // Ask the browser to lock the pointer
@@ -54,7 +53,6 @@ if ( havePointerLock ) {
 } else {
   instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
 }
-
 
 // Create the scene
 var camera, scene, renderer;
@@ -77,6 +75,7 @@ if (Detector.webgl) {
 function init() {
   // Setup camera
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+  // TODO : set cam. position
   scene = new THREE.Scene();
   scene.add ( camera );
 
@@ -99,11 +98,25 @@ function init() {
   raycaster = new THREE.Raycaster();
 
   // Setup floor
-  geometry = new THREE.PlaneBufferGeometry ( 20000, 20000, 100, 100 );
-  //geometry.rotateX( - Math.PI / 2 );
-  material = new THREE.MeshBasicMaterial( { color: 'skyblue' } );
-  floor = new THREE.Mesh( geometry, material );
-  scene.add( floor );
+  var texture = new THREE.TextureLoader().load( "media/ocean-texture.jpg" );
+  geometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
+  geometry.rotateX( - Math.PI / 2 );
+  // for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
+  //   var vertex = geometry.vertices[ i ];
+  //   vertex.x += Math.random() * 20 - 10;
+  //   vertex.y += Math.random() * 2;
+  //   vertex.z += Math.random() * 20 - 10;
+  // }
+  // for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
+  //   var face = geometry.faces[ i ];
+  //   face.vertexColors[ 0 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+  //   face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+  //   face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+  // }
+  material = new THREE.MeshBasicMaterial( { map: texture } );
+  mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
+
 
   // Setup renderer
   renderer = new THREE.WebGLRenderer();
@@ -111,13 +124,14 @@ function init() {
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
-  selecting = false;
 
   // Setup Mountains
   // TODO
 
   // Setup Scenery
   // TODO
+
+  window.addEventListener( 'resize', onWindowResize, false );
 }
 
 
@@ -126,4 +140,11 @@ function animate() {
   var center = new THREE.Vector2();
   raycaster.setFromCamera(center, camera);
   renderer.render( scene, camera );
+}
+
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
 }
